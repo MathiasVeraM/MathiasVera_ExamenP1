b8bea70d25c39d0e54d87b93336a9d35b0eb4e2e
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-10-29 07:29:51 -0500

    Clase MathiasVera creada, se incluyeron los atributos con los tipos especificados ademas de las anotaciones requeridas.

---
+++ MathiasVera_ExamenP1/Models/MathiasVera.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MathiasVera_ExamenP1.Models
+{
+    public class MathiasVera
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Es necesario de llenar el promedio")]
+        public decimal Promedio { get; set; }
+        [EmailAddress]
+        [MaxLength(70, ErrorMessage = "Ha excedido la cantidad maxima de caracteres")]
+        public string Correo { get; set; }
+        [AllowNull]
+        public bool TieneBeca { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
