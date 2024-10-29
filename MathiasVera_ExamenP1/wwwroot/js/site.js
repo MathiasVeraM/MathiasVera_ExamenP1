54b4ed30cac61aad699a4ce0e332bb0af77f000c
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-10-29 08:53:47 -0500

    Se editaron las vistas de Create, Edit e Index de las clases MathiasVera y Celulares. Ademas se creo la alerta y se agrego el meme y el boton en el layout

--- MathiasVera_ExamenP1/wwwroot/js/site.js
+++ MathiasVera_ExamenP1/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CrearAlerta() {
+    alert("Mi nombre es Mathias Vera y mi hobbie es Escuchar Musicales");
+}
