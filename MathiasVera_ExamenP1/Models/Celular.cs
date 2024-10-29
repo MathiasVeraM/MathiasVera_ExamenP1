6c0fc56f0f270d522c2767a1236a0c83580e0977
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-10-29 07:40:49 -0500

    Creacion de la segunda Clase Celular, junto con sus atributos correspondientes y las anotaciones requeridas

---
+++ MathiasVera_ExamenP1/Models/Celular.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MathiasVera_ExamenP1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Modelo { get; set; }
+        [DisplayName("Año del modelo")]
+        public int Año { get; set; }
+        [NotNull]
+        [AllowedValues(0.0, 1000.0)]
+        public decimal Precio { get; set; }
+        public MathiasVera Propietario { get; set; }
+        [ForeignKey("MathiasVera")]
+        public int MathiasVeraId { get; set; }
+
+    }
+}
